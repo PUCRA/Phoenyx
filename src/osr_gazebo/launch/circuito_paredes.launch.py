9fbbdcfa818eb5b4a9c28e64dee45d7b2a0aa141
Orioldcr <orioldcr@gmail.com>
2025-02-06 15:51:38 +0100

    fix: update world file path in circuito_paredes.launch.py

--- src/osr_gazebo/launch/circuito_paredes.launch.py
+++ src/osr_gazebo/launch/circuito_paredes.launch.py
@@ -10,7 +10,7 @@ import xacro
 
 def generate_launch_description():
     # Ruta al archivo de mundo directamente especificada
-    world_file = '/home/orioldcr/rover_ws/src/osr_gazebo/worlds/circuito_paredes.world'
+    world_file = './src/osr_gazebo/worlds/circuito_paredes.world'
 
     # Verificar que la ruta al archivo del mundo sea correcta
     print("Ruta al archivo del mundo:", world_file)
