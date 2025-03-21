f5c75a50273161bc7acd84d5d7b063d73dfd1b0e
SantiPallaresOcampo <santiago.pallares@estudiantat.upc.edu>
2025-03-21 10:48:40 +0100

    feat: :sparkles: Modificación launch efk

--- guiado/launch/efk.launch.py
+++ guiado/launch/efk.launch.py
@@ -1,7 +1,9 @@
 from launch import LaunchDescription
 from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+from launch.substitutions import LaunchConfiguration
 import os
+import launch
 
 def generate_launch_description():
     # Ruta al archivo de configuración YAML
@@ -10,8 +12,16 @@ def generate_launch_description():
         'config',
         'efk.yaml'
     )
+    
 
     return LaunchDescription([
+        Node(
+            package='robot_localization',
+            executable='ekf_node',
+            name='ekf_node',
+            output='screen',
+            parameters=[guiado_config, {'use_sim_time': LaunchConfiguration('use_sim_time')}]
+        ),
         Node(
             package='guiado',
             executable='localizacion_aruco',
@@ -20,4 +30,12 @@ def generate_launch_description():
             emulate_tty=True,
             parameters=[guiado_config],  # Carga el archivo YAML directamente
         ),
+        launch.actions.DeclareLaunchArgument(
+            name='use_sim_time', default_value='True',
+            description='Flag to enable use_sim_time'
+        ),
+        # robot_state_publisher_node,  # Todo esto que esta comentado lo indica en el tutorial, perono esta definido y no se como definirlo
+        # spawn_entity,
+        # robot_localization_node,
+        # rviz_node
     ])
