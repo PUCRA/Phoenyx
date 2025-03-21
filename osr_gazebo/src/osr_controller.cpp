20d1a572b0402ba38074c3533b26e1b85f34d22b
navilgarcia <sidi.navil.garcia@estudiantat.upc.edu>
2025-03-21 21:43:46 +0100

    definición del material "red"

--- osr_gazebo/src/osr_controller.cpp
+++ osr_gazebo/src/osr_controller.cpp
@@ -10,7 +10,11 @@
 
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "nav_msgs/msg/odometry.hpp"
+<<<<<<< HEAD
 #include <tf2_geometry_msgs/tf2_geometry_msgs.hpp>
+=======
+#include "tf2_geometry_msgs/tf2_geometry_msgs.hpp"
+>>>>>>> c53e81d (definición del material "red")
 
 #include "tf2/LinearMath/Quaternion.h"
 #include "tf2_ros/transform_listener.h"
