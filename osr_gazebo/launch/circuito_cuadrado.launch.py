fcbcaa35eba86a23e4a3975949486c8c0f98280e
Orioldcr <orioldcr@gmail.com>
2025-03-23 17:23:54 +0100

    map_guiado y circuito cuadrado cambiadas coordenadas (0, 0)

--- osr_gazebo/launch/circuito_cuadrado.launch.py
+++ osr_gazebo/launch/circuito_cuadrado.launch.py
@@ -60,11 +60,15 @@ def generate_launch_description():
         parameters=[{'use_sim_time': True}]  # use_sim_time agregado aquí también
     )
     
-    # Nodo para spawn de la entidad en Gazebo
+    # Nodo para spawn de la entidad en Gazebo con coordenadas
     spawn_entity = Node(
         package='gazebo_ros',
         executable='spawn_entity.py',
-        arguments=['-topic', 'robot_description', '-entity', 'rover'],
+        arguments=[
+            '-topic', 'robot_description',
+            '-entity', 'rover',
+            '-x', '1.0', '-y', '2.0', '-z', '0',  # Coordenadas de posición
+        ],
         output='screen',
         parameters=[{'use_sim_time': True}]  # use_sim_time agregado aquí también
     )
